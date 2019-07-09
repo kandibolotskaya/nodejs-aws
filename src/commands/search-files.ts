37a760c0590253eb080b4fae014067e686c90202
kandibolotskaya <kandibolotskaya.tatiana@gmail.com>
2019-07-09 11:03:13 +0300

    Implement SearchFiles command.

---
+++ src/commands/search-files.ts
@@ -0,0 +1,30 @@
+import { Command } from '@oclif/command';
+
+import { AWSError } from 'aws-sdk';
+
+import aws, { bucketParams } from '../aws';
+import { logWithPrettier, prettyJSON } from '../utils';
+
+export default class SearchFiles extends Command {
+  static description = 'List an AWS buckets files that match a "filter" regex';
+
+  static examples = [`$ nodejs-aws upload-file image.jpg`];
+
+  static args = [
+    { name: 'search', required: true, description: 'Search pattern' },
+  ];
+
+  async run() {
+    const { args } = this.parse(SearchFiles);
+
+    if (!args.search.length) {
+      this.error('Search pattern is empty.');
+    }
+
+    aws
+      .listObjects({ ...bucketParams, Prefix: args.search })
+      .promise()
+      .then(logWithPrettier(this.log, prettyJSON))
+      .catch((err: AWSError) => this.error(err));
+  }
+}
