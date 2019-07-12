003e5ead645e0ce85bff854cf35cbdb4446a9c59
kandibolotskaya <kandibolotskaya.tatiana@gmail.com>
2019-07-12 16:29:18 +0300

    adding pagination

--- src/aws.ts
+++ src/aws.ts
@@ -4,8 +4,11 @@ import config from './config';
 
 export const bucketParams = {
   Bucket: config.bucketName,
+  Marker: config.marker,
 };
 
 export default new AWS.S3({
   apiVersion: config.apiVersion,
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
 });
--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,15 @@ const config = require('dotenv').config();
 export interface Config {
   bucketName: string;
   apiVersion: string;
+  accessKeyId: string;
+  secretAccessKey: string;
+  marker: number;
 }
 
 export default {
   bucketName: config.parsed.BUCKET_NAME,
   apiVersion: config.parsed.API_VERSION,
+  accessKeyId: config.parsed.ACCESS_KEY,
+  secretAccessKey: config.parsed.SECRET_KEY,
+  marker: config.parsed.MARKER,
 } as Config;
